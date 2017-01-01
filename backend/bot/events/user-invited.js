8f24546b8bb4b67b23f783f4b30fc54780010443
Oleg N <75450326+olnazx@users.noreply.github.com>
2017-01-01 12:00:00 +0300

    @olnazx/vk-chatbot-lite

---
+++ backend/bot/events/user-invited.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ * @private
+ */
+const pino = require('../../lib/pino');
+
+/**
+ * Local constants.
+ * @private
+ */
+
+/**
+ * Список ID наших ботов.
+ * @type {Array of String}
+ */
+const OUR_BOTS_IDS = Object.keys(require('../../config/accounts'));
+
+/**
+ * В беседу пригласили пользователя.
+ * @param {Bot} bot Экземпляр "Bot"
+ * @param {Object} message Объект сообщения
+ * @public
+ */
+async function handler (bot, message) {
+  // В беседу был приглашён ещё один наш бот.
+  // Старый бот выходит, новый остаётся.
+  if (OUR_BOTS_IDS.includes(message.attachments.source_mid.toString())) {
+    return bot.api.call('messages.removeChatUser', {
+        chat_id: message.conversation_id,
+        user_id: bot.id
+      })
+      .then(() => null)
+      .catch(error => {
+        if (error.name === 'VkApiError') {
+
+          // Нет доступа к чату. Значит, бот уже вышел из него.
+          if (error.code === 15) {
+            return;
+          }
+        }
+
+        pino.error('Bot id%d unable to leave a chat.', bot.id, error);
+
+        return;
+      });
+  }
+
+  return;
+}
+
+module.exports = handler;
---
+++ backend/lib/cleverbot.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ * @private
+ */
+const { URLSearchParams } = require('url');
+const xml2js = require('xml2js');
+const fetch = require('./fetch');
+const config = require('../config');
+
+/**
+ * Local constants.
+ * @private
+ */
+const REQUEST_URL = 'http://cleverbot.existor.com/webservicexml';
+
+/**
+ * Парсит ответ Cleverbot.
+ * @param {String} response XML-документ
+ * @returns {Promise}
+ * @private
+ */
+async function parseResponse (response) {
+  if (!response || !response.length) {
+    return;
+  }
+
+  return new Promise((resolve, reject) => {
+    xml2js.parseString(response, (err, result) => {
+      if (err) {
+        // Не возвращаем и не логгируем ошибки парсера.
+        return resolve();
+      }
+
+      resolve(result.webservicexml.session[0].response[0]);
+    });
+  });
+}
+
+/**
+ * Получает ответ на сообщение.
+ * @param {String} messageText Текст сообщения
+ * @returns {Promise}
+ * @public
+ */
+async function send (messageText) {
+  if (!messageText) {
+    return;
+  }
+
+  return fetch(REQUEST_URL, {
+      method: 'POST',
+      body: new URLSearchParams({
+        icognoID: config['cleverbot-username'],
+        icognoCheck: config['cleverbot-password'],
+        isLearning: '0',
+        stimulus: messageText
+      })
+    })
+    .then(response => response.text())
+    .then(response => parseResponse(response));
+}
+
+module.exports = {
+  send
+}
